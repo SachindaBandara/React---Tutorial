9f5fabc259fb8db7660a9a20b11d892d69ddb0b9
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-25 15:53:08 +0530

    add

--- NestedCreateElement.js
+++ NestedCreateElement.js
@@ -1,25 +1,36 @@
 {
   /* 
     <div id= "root">
+
          <div id="parent">
-            <div id="child">
+            <div id="child1">
                  <h1>I am sachinda</h1>
                  <h2>I am ravindu</h2>
             </div>
         </div>
+
+        <div id="parent">
+            <div id="child2">
+                 <h1>I am sachinda</h1>
+                 <h2>I am ravindu</h2>
+            </div>
+        </div>
+
     </div>
 
 */
 }
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am sachinda"),
     React.createElement("h2", {}, "I am ravindu"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am sachinda"),
+    React.createElement("h2", {}, "I am ravindu"),
+  ]),
+]);
 
 console.log(parent);
 
