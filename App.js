a9b3e4b0f4090a86c147dd2145b54a02b07bf191
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-11-21 13:06:52 +0530

    JSX intro

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create react element
 // heading is an object
+//React.createElement => create ReactElement- JS object => HTMLElement(render)
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -7,6 +12,21 @@ const heading = React.createElement(
 
 console.log(heading); // object
 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); // object is converted code that browser understandable(HTMLDOM)
+
+//=====================================
+//=====================================
+// JSX - It is like HTML, XML syntax
+// Create JSX element
+// JSX => React.createElement => create ReactElement- JS object => HTMLElement(render)
+
+const jsxHeading = <h1 id="heading">Hello React!</h1>;
+
+console.log(jsxHeading); // object
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); // object is converted code that browser understandable
+root.render(jsxHeading); // object is converted code that browser understandable(HTMLDOM)
+
