b4a6804ab88c3a35e6bed8fb2cd2bd5195481aa2
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-11-21 14:23:15 +0530

    Components

--- App.js
+++ App.js
@@ -26,7 +26,44 @@ const jsxHeading = <h1 id="heading">Hello React!</h1>;
 
 console.log(jsxHeading); // object
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(jsxHeading); // object is converted code that browser understandable(HTMLDOM)
+
+
+//=====================================
+//=====================================
+// React Functional Component
+// method 1
+const HeadingComponent = () => {
+  return <h1>Hello world React!</h1>;
+}
+// method 2
+const HeadingComponent2 = () => <h1>Hello world React!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>); //functional component rendering
+
+
+// display react element using {}
+      const title = (
+        <h1>Hello React</h1>
+      );
+
+      const Title1 = () => (
+        <h1>Hello React</h1>
+      ); 
+
+      const HeadingComponent3 = () => {
+        <div>
+          {title}
+          {Title1()}
+          <Title1/>
+          <h1>Hi I am Sachinda</h1>
+        </div>
+      };
+      //  {Title1()} = <Title1/>
+
+
 
